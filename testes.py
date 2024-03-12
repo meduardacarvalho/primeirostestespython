24525dfcd6d4646128a81c21cee4faebda8febb0
meduardacarvalho <126161075+meduardacarvalho@users.noreply.github.com>
2024-03-11 21:01:21 -0300

    Add files via upload

---
+++ testes.py
@@ -0,0 +1,15 @@
+print("olá mundo")
+print("Olá, mundo!")
+7+4
+print(7+4)
+print("7" + "4")
+print ("olá", 5)
+nome1 = "duda"
+idade = 20
+peso = 60
+print("nome: ", nome1 , "idade:", idade , "peso:", peso ) 
+nome2 = input("qual seu nome")
+idade2 =input("qual sua idade")
+peso2 = input("qual seu peso")
+print("parabéns", nome2 ,"sua idade é ",  idade2 , " e seu peso" , peso2)
+
